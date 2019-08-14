cae55b6853fc4da21634676f92d3155ce1bfabd6
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-14 13:31:29 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  60 < speed || speed < 40 ? true : false
 end
-	
-
-
